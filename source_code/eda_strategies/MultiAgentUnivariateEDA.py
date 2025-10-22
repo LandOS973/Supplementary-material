35d996a99e010e58db663b54365975b391719b3e
LandOS973 <76964599+LandOS973@users.noreply.github.com>
2025-10-22 15:35:16 +0200

    algo reinforce pour univariatePPO
    todo: revoir baseline et implementer ppo (demander a olivier pour si on laisse seulement reinforce ou ppo)

--- source_code/eda_strategies/MultiAgentUnivariateEDA.py
+++ source_code/eda_strategies/MultiAgentUnivariateEDA.py
@@ -19,16 +19,13 @@ class UnivariatePPOEDA(Abstract_EDA, nn.Module):
         self.learning_rate = learning_rate
         self.dim_variables = dim_variables
         self.lambda_ = lambda_
-        self.N = N
+        self.N = N # nombre de variables
         self.device = device
         self.beta = beta
         self.theta = None
-        self.logits = None
         self.optimizerG = None
-        self.agent_number = agent_number 
-        self.theta_old = None
-
-
+        self.baseline = None
+        self.agent_number = agent_number
     def forward(self):
         """
         Retourne les probabilités p(x_i=1) = sigmoid(theta_i)
@@ -40,7 +37,6 @@ class UnivariatePPOEDA(Abstract_EDA, nn.Module):
     def reset_learned_parameters(self, nb_instances):
         """Réinitialise les paramètres et sauvegarde le nombre d'instances."""
         self.theta = nn.Parameter(torch.zeros((nb_instances, self.N), device=self.device))
-        self.theta_old = self.theta.clone().detach()
         self.nb_instances = nb_instances
         self.optimizerG = torch.optim.Adam([self.theta], lr=self.learning_rate)
 
@@ -56,38 +52,115 @@ class UnivariatePPOEDA(Abstract_EDA, nn.Module):
             samples = torch.bernoulli(probs_pop).unsqueeze(-1)             # (nb_instances, λ, N, 1)
 
         return samples.to(self.device)
+    
+
+    # Version non optimisée pour la fonction de perte
+    # def updateDistribution(self, solutionList, scoreList):
+    #     device = self.device
+    #     solutionList = solutionList.to(device)
+    #     scoreList = scoreList.to(device)
+    #     solutions = solutionList.squeeze(-1)  # (nb_instances, λ, N)
+    #     # X => Individu ayant N variables {-1,1} [x1, x2, ..., xN] => exemple dans QUBO 64, chaque individu est une solution de 64 variables
+    #     X = solutions  # (nb_instances, λ, N)
+    #     # i indice sur l'individu Xi [indi 1, indi 2, ..., indi λ]
+    #     # k indice sur la variable de l'individu i [x1, x2, xK, xN] de l'individu Xi
+
+    #     # L(Theta) = (1/self.lamba_) * sum( de i=1 a self.lambda_) [ fitness(Xi) * sum( de k=1 a N ) [ log(Sigmoid(Theta k)) si a[k]=1 et log(1-Sigmoid(Theta k)) si a[k]=-1 ] ]
+    #     # on fait les descentes de gradient des L(Theta_i) en parallèle pour chaque instance i a la fin des itérations
+    #     total_loss = 0.0
+    #     # probs = theta a qui on applique sigmoid pour avoir les proba de chaque variable
+    #     probs = self.forward()  # (nb_instances, N)
+    #     for n in range(self.nb_instances):
+    #         #sum( de i=1 a self.lambda_)
+    #         loss_n = 0.0
+    #         # probabilités pour chaque variable de l'instance n
+    #         # Theta => [Theta1, Theta2, ..., ThetaN] vecteur des probabilités pour chaque variable
+    #         Pi_Theta = probs[n]  # (N,)
+    #         # maintenant, on rentre dans chaque individu de l'instance n
+    #         for i in range(self.lambda_): # i => indice sur l'individu {0,...,self.lambda_-1}
+    #             # self.lambda_ => nombre d'individus samplés
+    #             All_Pi_Theta_k_a_i_k = []
+    #             # Log(Pi(Theta X)) = sum(log(Pi(Theta k)(a[k]))) = sum( log(Sigmoid(Theta k)) si a[k]=1 et log(1-Sigmoid(Theta k)) si a[k]=-1 )
+    #             log_Pi_Theta_X = 0.0
+                
+    #             # on rentre maintenant dans chaque variable de l'individu k de l'instance n
+    #             for k in range(self.N): # k => indice sur la variable {0,...,N-1} (64 dans QUBO 64)
+    #                 # a[i][k] => action choisie pour la variable k de l'individu i {0,1} => valeur tirée par la proba (convertie plus tard en {-1,1} pour le calcul de la fitness)
+    #                 a_i_k = X[n, i, k]
+    #                 # Pi(Theta k) => proba de choisir 1 pour la variable k pour chaque individu
+    #                 Pi_Theta_k = Pi_Theta[k]
+    #                 # Pi(Theta k)(a[i][K]) => proba de choisir l'action a[i][k] pour chaque individu equivaut ici a Sigmoid(theta k) si a[k]=1 et 1-Sigmoid(theta k) si a[k]=-1
+    #                 # Pi_Theta = sigmoid(theta)
+    #                 Pi_Theta_k_a_i_k = Pi_Theta_k if a_i_k.item() == 1.0 else (1.0 - Pi_Theta_k)
+    #                 log_Pi_Theta_X += torch.log(Pi_Theta_k_a_i_k)
+    #                 All_Pi_Theta_k_a_i_k.append(Pi_Theta_k_a_i_k)
+                
+    #             # Pi(Theta X) => proba de trouver l'individu X = Pi(Theta1)(a[1]) * Pi(Theta2)(a[2]) * ... * Pi(ThetaN)(a[N])
+    #             # Pi_Theta_X = torch.prod(torch.stack(All_Pi_Theta_k_a_i_k))
+    #             fitness_X_i = scoreList[n, i]
+    #             # fitness(Xi) * sum( de k=1 a N ) [ log(Sigmoid(Theta k)) si a[k]=1 et log(1-Sigmoid(Theta k)) si a[k]=-1 ] ]
+    #             loss_n += fitness_X_i * log_Pi_Theta_X
+    #         # J(Theta) => Esperance de la trajectoire générée par Pi(Theta X) sur la fitness
+    #         # On veut maximiser J(Theta)
+    #         # J(Theta) = Esperance~PiTheta [fitness(X)] => 1/self.lambda_ * sum( de i=1 a self.lambda_) [ fitness(Xi) ]
+    #         total_loss += loss_n
+
+    #     # L(Theta) = (1/self.lamba_) * sum( de i=1 a self.lambda_) [ fitness(Xi) * sum( de k=1 a N ) [ log(Sigmoid(Theta k)) si a[k]=1 et log(1-Sigmoid(Theta k)) si a[k]=-1 ] ]
+    #     total_loss /= self.nb_instances
+    #     self.optimizerG.zero_grad()
+    #     (-total_loss).backward()
+    #     self.optimizerG.step()
+    #     return total_loss
 
     def updateDistribution(self, solutionList, scoreList):
+    #     # X => Individu ayant N variables {-1,1} [x1, x2, ..., xN] => exemple dans QUBO 64, chaque individu est une solution de 64 variables
+    #     # X = solutions  # (nb_instances, λ, N)
+    #     # i indice sur l'individu Xi [indi 1, indi 2, ..., indi λ]
+    #     # k indice sur la variable de l'individu i [x1, x2, xK, xN] de l'individu Xi
         device = self.device
         solutionList = solutionList.to(device)
         scoreList = scoreList.to(device)
-        solutions = solutionList.squeeze(-1)  # (nb_instances, λ, N)
+        X = solutionList.squeeze(-1)  # (nb_instances, λ, N)
+        probs = self.forward()        # (nb_instances, N)
 
-        print("les thetas avant updateDistribution", torch.sigmoid(self.theta))
-        print("nb de proba" , self.theta.shape)
+        L_Theta = 0.0
 
-        # i indice sur l'individu Xi [indi 1, indi 2, ..., indi λ]
-        # k indice sur la variable de l'individu i [x1, x2, xK, xN] de l'individu Xi
-        # X => Individu ayant N variables {-1,1} [x1, x2, ..., xN] => exemple dans QUBO 64, chaque individu est une solution de 64 variables
-        # self.lambda_ => nombre d'individus samplés
-        # a[i][k] => action choisie pour la variable k de l'individu i {-1,1} => valeur tirée par la proba
-        # Pi(Theta k) => proba de choisir 1 pour la variable k pour chaque individu
-        # Pi(Theta k)(a[K]) => proba de choisir l'action a[k] pour chaque individu equivaut ici a Sigmoid(theta k) si a[k]=1 et 1-Sigmoid(theta k) si a[k]=-1
-        # Tetha => [Tetha1, Theta2, ..., ThetaN] vecteur des probabilités pour chaque variable
-        # Pi(Theta X) => proba de trouver l'individu X = Pi(Theta1)(a[1]) * Pi(Theta2)(a[2]) * ... * Pi(ThetaN)(a[N])
-        # J(Theta) => Esperance de la trajectoire générée par Pi(Theta X) sur la fitness
-        # On veut maximiser J(Theta)
-        # J(Theta) = Esperance~PiTheta [fitness(X)]
-        # Gradient de J(Theta) = Esperance~PiTheta [fitness(X) * grad( log(Pi(Theta X)) )]
-        # grad( log(Pi(Theta X)) ) est inaccessible, on l'estime par Monte Carlo
-        # L(Theta) est l'estimation empirique de Gradient de J(Theta)
-        # Log(Pi(Theta X)) = sum(log(Pi(Theta k)(a[k]))) = sum( log(Sigmoid(Theta k)) si a[k]=1 et log(1-Sigmoid(Theta k)) si a[k]=-1 )
-        # L(Theta) = (1/self.lamba_) * sum( de i=1 a self.lambda_) [ fitness(Xi) * sum( de k=1 a N ) [ log(Sigmoid(Theta k)) si a[k]=1 et log(1-Sigmoid(Theta k)) si a[k]=-1 ] ]
-        # une fois L(Theta calculé) calculer du gradient par L.backward()
-        # self.optimizerG.step() pour faire un pas de gradient
-        
+        for n in range(self.nb_instances):
+            Pi_Theta = probs[n]  # (N,)
+            # on agrandit Pi_Theta sous la forme (λ, N) pour faire les opérations par lot
+            # -1 signifie que la dimension N reste inchangée
+            # unsqueeze(0) ajoute une dimension en position 0 pour la rempire avec λ
+            Pi_Theta_expanded = Pi_Theta.unsqueeze(0).expand(self.lambda_, -1)  # (λ, N)
+            actions = X[n]  # (λ, N)
+            # f(Xi) pour chaque individu i de l'instance n
+            fitness = scoreList[n]  # (λ,)
+
+            # Log(Pi(Theta X)) = sum(log(Pi(Theta k)(a[k]))) = sum( log(Sigmoid(Theta k)) si a[k]=1 et log(1-Sigmoid(Theta k)) si a[k]=-1 )
+            Pi_selected = torch.where(actions == 1.0, Pi_Theta_expanded, 1.0 - Pi_Theta_expanded)  # (λ, N)
+            log_Pi = torch.log(Pi_selected).sum(dim=1) 
+
+            # L(θ) = moyenne des fitness pondérées par log π
+            
+            if self.baseline is None:
+                loss_n = torch.mean(fitness * log_Pi)
+            else:
+                loss_n = torch.mean((fitness - self.baseline) * log_Pi)
+            self.baseline = fitness.mean().item()
+
+            L_Theta += loss_n
+        # L(Theta) = (1/self.lambda_) * sum( de i=1 a self.lambda_) [ fitness(Xi) * sum( de k=1 a N ) [ log(Sigmoid(Theta k)) si a[k]=1 et log(1-Sigmoid(Theta k)) si a[k]=-1 ] ]
+        L_Theta /= self.nb_instances
+        self.optimizerG.zero_grad()
+        (-L_Theta).backward()
+        self.optimizerG.step()
+        return L_Theta.item()
 
 
+   
+                
+
+
+            
 
 
     def toString(self):
