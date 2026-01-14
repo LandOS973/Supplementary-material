f6163c9cb353ebd1636465248bfaa340c311658a
LandOS973 <76964599+LandOS973@users.noreply.github.com>
2026-01-14 10:11:49 +0100

    Update SLURM output path and modify QUBO problem dimensions in main_expe.py

--- source_code/main_expe.py
+++ source_code/main_expe.py
@@ -34,7 +34,7 @@ PROBLEMS = [
     dict(name="QUBO", dim=64, type_instance=1),
     dict(name="QUBO", dim=64, type_instance=2),
     dict(name="QUBO", dim=128, type_instance=0),
-    dict(name="QUBO", dim=128, type_instance=1)
+    dict(name="NK", dim=256, type_instance=0),
 ]
 
 DEFAULTS = dict(
